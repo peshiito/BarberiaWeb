c2a3cc6b9250c216ca123c9c2583d56919457529
peshiito <pgbaez0@gmail.com>
2026-07-05 18:44:47 -0300

    restructuracion del proyecto

--- Backend/src/controllers/index.ts
+++
@@ -1,8 +0,0 @@
-// backend/src/controllers/index.ts
-export * from "./admin.controller";
-export * from "./appointment.controller";
-export * from "./auth.controller";
-export * from "./barber.controller";
-export * from "./photo.controller";
-export * from "./schedule.controller";
-export * from "./service.controller";
--- Backend/src/middlewares/rateLimit.middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-import { createRateLimiter } from "../config/rateLimit";
-
-export const rateLimitMiddleware = (windowMs?: number, max?: number) => {
-    const limiter = createRateLimiter(windowMs, max);
-    return (req: Request, res: Response, next: NextFunction) => {
-        return limiter(req, res, next);
-    };
-};
